84e3d8b6e5ddb92c0b3e35b35742b73b7a57b21f
K08 <162245014+kgriffi08@users.noreply.github.com>
2024-05-10 20:30:08 -0400

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Demain, dès l'aube, à l'heure où blanchit la campagne",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
